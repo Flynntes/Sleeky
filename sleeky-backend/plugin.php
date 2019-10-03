fd530151955ee58d43340d0e73ada38304c50da2
Flynn Tesoriero <flynn@flynntes.com>
2019-10-03 17:42:40 +1000

    Incremented backend version

--- sleeky-backend/plugin.php
+++ sleeky-backend/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Sleeky Backend
 Plugin URI: https://sleeky.flynntes.com
 Description: UI overhaul of the YOURLS backend
-Version: 3.0
+Version: 3.1
 Author: Flynn Tesoriero
 Author URI: https://flynntes.com
 */
